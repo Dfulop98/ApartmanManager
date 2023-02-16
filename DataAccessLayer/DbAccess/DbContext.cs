70f29ee23f366f07863ffacf5158d9125ad20ab4
Dfulop98 <dobo.fulop@icloud.com>
2023-02-16 12:43:55 +0100

    Add: create a base file architecture.

---
+++ DataAccessLayer/DbAccess/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using EFDataAccessLibrary.Models.ModelInterfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace DataAccessLayer.DbAccess
+{
+    internal class DbContext
+    {
+    }
+}
---
+++ ServiceLayer/ServiceInterfaces/IBookingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLayer.Models;
+namespace ServiceLayer.ServiceInterfaces
+{
+    public interface IBookingService
+    {
+        public void AddReservation();
+        public void RemoveReservation();
+        public void UpdateReservation();
+
+        public List<Reservation> GetReservations();
+        public Reservation GetReservationById(int id);
+    }
+}
