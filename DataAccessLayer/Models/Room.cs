0084f80117b822e372e1501890afaf71fa12b881
Dfulop98 <dobo.fulop@icloud.com>
2023-02-20 19:38:51 +0100

    Feat: Room entity model

--- DataAccessLayer/Models/Room.cs
+++ DataAccessLayer/Models/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace DataAccessLayer.Models
 {
     public class Room
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoomNumber { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; }
+
+        [Required]
+        public decimal PricePerNight { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
     }
 }
