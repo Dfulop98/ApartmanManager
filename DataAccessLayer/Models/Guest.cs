4cd5cb667d01011dcd75efa707cd44e24ae23e33
Dfulop98 <dobo.fulop@icloud.com>
2023-02-20 20:03:59 +0100

    Feat: Guest entity model

--- DataAccessLayer/Models/Guest.cs
+++ DataAccessLayer/Models/Guest.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DataAccessLayer.Models
 {
     public class Guest
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nationatily { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
     }
 }
