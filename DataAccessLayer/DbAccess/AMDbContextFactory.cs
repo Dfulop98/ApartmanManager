ce7d16e7d9639dcad0f18522456ae0dde98e9e3b
Dfulop98 <dobo.fulop@icloud.com>
2023-02-20 19:11:05 +0100

    Feat : Dbcontext factory

---
+++ DataAccessLayer/DbAccess/AMDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer.DbAccess
+{
+    public class AMDbContextFactory : IDesignTimeDbContextFactory<AMDbContext>
+    {
+        
+        public AMDbContext CreateDbContext(string[] args) 
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../ApartmanManagerApi"))
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AMDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new InvalidOperationException("Connection string is null or empty.");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new AMDbContext(optionsBuilder.Options);
+        }
+        
+    }
+}
