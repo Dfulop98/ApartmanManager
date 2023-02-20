f834696670b1fd4b432f371a4280f7fdd2969f73
Dfulop98 <dobo.fulop@icloud.com>
2023-02-20 19:08:50 +0100

    Fix: rename web api project

--- ApartmanManagerApi/Program.cs
+++ ApartmanManagerApi/Program.cs
@@ -2,23 +2,21 @@
 using DataAccessLayer.DbAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.InMemory;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 // Add services to the container.
+
+builder.Services.AddSingleton(builder.Configuration);
+builder.Services.AddDbContext<AMDbContext>();
+
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<AMDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
