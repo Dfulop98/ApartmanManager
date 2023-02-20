7d2251545c0c74b0f0a57b9a061bb40e49bdf39f
Dfulop98 <dobo.fulop@icloud.com>
2023-02-20 20:03:47 +0100

    Feat: payment entity model

--- DataAccessLayer/Models/Payment.cs
+++ DataAccessLayer/Models/Payment.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLayer.Models
 {
     public class Payment
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PaymentDate { get; set; }
+
+        [Required]
+        public int ReservationId { get; set; }
+
+        [ForeignKey("ReservationId")]
+        public Reservation Reservation { get; set; }
     }
 }
