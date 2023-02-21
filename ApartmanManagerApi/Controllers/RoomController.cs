f24736468cbc51bcbbbcf00b8d1a5fe93db66c67
Dfulop98 <dobo.fulop@icloud.com>
2023-02-21 23:30:48 +0100

    Feat: GetAllRooms method

---
+++ ApartmanManagerApi/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.DbAccess;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.ServiceInterfaces;
+
+namespace ApartmanManagerApi.Controllers
+{
+    public  class RoomController : ControllerBase
+
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet("/api/room")]
+        public async Task<ActionResult<IEnumerable<Room>>> GetAllRooms()
+        {
+            return Ok(await _roomService.GetAllRoomAsync());
+        }
+
+
+    }
+}
