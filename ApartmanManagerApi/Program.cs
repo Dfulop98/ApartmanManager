d97676d33567fe57f13148e8f923ed6fd6ad9693
Dfulop98 <dobo.fulop@icloud.com>
2023-02-21 23:31:38 +0100

    Update: references ,Nuget packages and DI

--- ApartmanManagerApi/Program.cs
+++ ApartmanManagerApi/Program.cs
@@ -2,13 +2,14 @@
 using DataAccessLayer.DbAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore.InMemory;
-
-
+using ApartmanManagerApi.Controllers;
+using ServiceLayer.ServiceInterfaces;
+using ServiceLayer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 // Add services to the container.
+
 
 builder.Services.AddSingleton(builder.Configuration);
 builder.Services.AddDbContext<AMDbContext>();  
@@ -17,6 +18,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddTransient<IRoomService, RoomService> ();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,5 +35,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
