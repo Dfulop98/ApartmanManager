6ab0b1e41f958c9c79da36f026f1b4f4bb50e23c
Dfulop98 <dobo.fulop@icloud.com>
2023-02-21 23:33:22 +0100

    Fix: fix namespace typo and encapsulations

--- ServiceLayer/Services/PaymentService.cs
+++ ServiceLayer/Services/PaymentService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Services
+namespace ServiceLayer.Services
 {
     internal class PaymentService
     {
