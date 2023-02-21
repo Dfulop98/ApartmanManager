2cee679409383738bb7a03ecfb9a32e8446abd3b
Dfulop98 <dobo.fulop@icloud.com>
2023-02-21 11:14:44 +0100

    Feat: create a proper connection between app and database

--- ApartmanManagerApi/Program.cs
+++ ApartmanManagerApi/Program.cs
@@ -11,7 +11,7 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 // Add services to the container.
 
 builder.Services.AddSingleton(builder.Configuration);
-builder.Services.AddDbContext<AMDbContext>();
+builder.Services.AddDbContext<AMDbContext>();  
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
