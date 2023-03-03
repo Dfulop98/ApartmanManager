857f13ef66e2fad888a4bcbd05d8269f869105ef
Dfulop98 <dobo.fulop@icloud.com>
2023-03-03 13:18:49 +0100

    Add: implement first tests for add reservation

--- Tests/ReservationServiceTests.cs
+++ Tests/ReservationServiceTests.cs
@@ -1,8 +1,11 @@
 ﻿using DataAccessLayer.DbAccess;
+using DataModelLayer.Models;
 using Microsoft.EntityFrameworkCore;
+using ServiceLayer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,25 +28,146 @@ namespace Tests
         [Test]
         public async Task AddReservationAsync_WithValidReservationData_ReturnsSuccess()
         {
-
+            using  (_db)
+            {
+                //Arrage
+                _db.Rooms.AddRange(new List<Room>
+                {
+                    new Room { Id = 1, RoomNumber = "101", Description="test1", IsAvailable = false },
+                    new Room { Id = 2, RoomNumber = "102", Description="test2", IsAvailable = true },
+                    new Room { Id = 3, RoomNumber = "103", Description="test3", IsAvailable = false }
+                });
+                ReservationService service = new(_db);
+                DateTime checkinDate = new (2008, 5, 1, 8, 30, 52);
+                DateTime checkoutDate = new (2008, 5, 4, 8, 30, 52);
+                Reservation newReservation = new()
+                {
+                    Name = "test name",
+                    Email = "test@email.com",
+                    Phone = "+36302642038",
+                    CheckInDate = checkinDate,
+                    CheckOutDate = checkoutDate,
+                    NumberOfGuests = 1,
+                    RoomId = 1,
+                };
+                //Act
+                HttpResponseMessage result = await service.AddReservationAsync(newReservation);
+                //Assert
+                Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+            }
         }
         
         [Test]
         public async Task AddReservationAsync_WithExistingReservation_ReturnsConflict()
         {
-
+            using (_db)
+            {
+                //Arrage
+                _db.Rooms.AddRange(new List<Room>
+                {
+                    new Room { Id = 1, RoomNumber = "101", Description="test1", IsAvailable = false },
+                    new Room { Id = 2, RoomNumber = "102", Description="test2", IsAvailable = true },
+                    new Room { Id = 3, RoomNumber = "103", Description="test3", IsAvailable = false }
+                });
+                ReservationService service = new(_db);
+                DateTime checkinDate = new(2008, 5, 1, 8, 30, 52);
+                DateTime checkoutDate = new(2008, 5, 4, 8, 30, 52);
+                _db.Reservations.AddRange(new List<Reservation>
+                {
+                    new Reservation
+                    {
+                        Name = "test name",
+                        Email = "test@email.com",
+                        Phone = "+36302642038",
+                        CheckInDate = checkinDate,
+                        CheckOutDate = checkoutDate,
+                        NumberOfGuests = 1,
+                        RoomId = 1,
+                    },
+                    new Reservation
+                    {
+                        Name = "test name2",
+                        Email = "test2@email.com",
+                        Phone = "+36302642037",
+                        CheckInDate = checkinDate,
+                        CheckOutDate = checkoutDate,
+                        NumberOfGuests = 2,
+                        RoomId = 2,
+                    },
+                });
+                Reservation newReservation = new()
+                {
+                    Name = "test name",
+                    Email = "test@email.com",
+                    Phone = "+36302642038",
+                    CheckInDate = checkinDate,
+                    CheckOutDate = checkoutDate,
+                    NumberOfGuests = 1,
+                    RoomId = 1,
+                };
+                //Act
+                HttpResponseMessage result = await service.AddReservationAsync(newReservation);
+                //Assert
+                Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Conflict));
+            }
         }
         
         [Test]
         public async Task AddReservationAsync_WithInvalidReservationData_ReturnsBadRequest()
         {
-
+            using (_db)
+            {
+                //Arrage
+                _db.Rooms.AddRange(new List<Room>
+                {
+                    new Room { Id = 1, RoomNumber = "101", Description="test1", IsAvailable = false },
+                    new Room { Id = 2, RoomNumber = "102", Description="test2", IsAvailable = true },
+                    new Room { Id = 3, RoomNumber = "103", Description="test3", IsAvailable = false }
+                });
+                ReservationService service = new(_db);
+                DateTime checkinDate = new(2024, 5, 1, 8, 30, 52);
+                DateTime checkoutDate = new(2008, 5, 4, 8, 30, 52);
+                Reservation newReservation = new()
+                {
+                    Name = "123",
+                    Email = "test@email.com",
+                    Phone = "+36302642038",
+                    CheckInDate = checkinDate,
+                    CheckOutDate = checkoutDate,
+                    NumberOfGuests = 1,
+                    RoomId = 1,
+                };
+                //Act
+                HttpResponseMessage result = await service.AddReservationAsync(newReservation);
+                //Assert
+                Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            }
         }
         
         [Test]
         public async Task AddReservationAsync_WithNonExistentRoom_ReturnsNotFound()
         {
-
+            using (_db)
+            {
+                //Arrage
+                ReservationService service = new(_db);
+                DateTime checkinDate = new(2008, 5, 1, 8, 30, 52);
+                DateTime checkoutDate = new(2008, 5, 4, 8, 30, 52);
+                Reservation newReservation = new()
+                {
+                    Name = "123", 
+                    Email = "test@email.com",
+                    Phone = "+36302642038",
+                    CheckInDate = checkinDate,
+                    CheckOutDate = checkoutDate,
+                    NumberOfGuests = 1,
+                    RoomId = 20,
+                };
+                //Act
+                HttpResponseMessage result = await service.AddReservationAsync(newReservation);
+                //Assert
+                Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            }
         }
     }
 }
