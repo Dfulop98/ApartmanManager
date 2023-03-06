d2715d6e123f13b44da23f03947592847bb02baa
Dfulop98 <dobo.fulop@icloud.com>
2023-03-06 19:14:12 +0100

    Add: api endpoint for whole reservation controller(some forgotten from previous branches)

---
+++ ApartmanManagerApi/Controllers/ReservationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.ServiceInterfaces;
+using DataModelLayer.Models;
+using System.Collections.Generic;
+
+namespace ApartmanManagerApi.Controllers
+{
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservatonService _reservatonService;
+        public ReservationController(IReservatonService reservatonService) 
+        {
+            _reservatonService = reservatonService;
+        }
+
+        [HttpGet("/api/reservations")]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetAllReservations()
+        {
+
+        } 
+        
+        [HttpGet("/api/reservation/{id}")]
+        public async Task<ActionResult<Reservation>> GetReservationById(int id)
+        {
+
+        }
+
+        [HttpPost("/api/reservation/add")]
+        public async Task<ActionResult> AddReservation()
+        {
+
+        }
+
+        [HttpPut("/api/reservation/update")]
+        public async Task<ActionResult> UpdateReservation()
+        {
+
+        }
+        
+        [HttpDelete("/api/reservation/remove/{id}")]
+        public async Task<ActionResult> DeleteRoom(int id)
+        {
+
+        }
+    }
+}
