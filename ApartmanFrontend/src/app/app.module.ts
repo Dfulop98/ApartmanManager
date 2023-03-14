48c17509613752fa2bd291f8187f4f10d19f764e
Dfulop98 <dobo.fulop@icloud.com>
2023-03-14 15:26:32 +0100

    Add: Frontend stuffs

--- ApartmanFrontend/src/app/app.module.ts
+++ ApartmanFrontend/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomService } from './rooms/rooms.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    RoomService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
