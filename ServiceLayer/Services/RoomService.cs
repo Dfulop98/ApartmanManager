2e9d9dd6cc410e41c826919011f99ff324ecd35e
Dfulop98 <dobo.fulop@icloud.com>
2023-03-14 15:24:49 +0100

    Refactor: Room service method return type change

--- ServiceLayer/Services/RoomService.cs
+++ ServiceLayer/Services/RoomService.cs
@@ -8,85 +8,89 @@ using System.Net;
 using DataAccessLayer.Operations;
 using DataAccessLayer.Interfaces;
 using System.ComponentModel;
-using ServiceLayer.Models;
+using ServiceLayer.Factories.Model;
+using ServiceLayer.Factories.Interfaces;
 
 namespace ServiceLayer.Services
 {
     public class RoomService : IRoomService
     {
         private readonly IRoomDataAccess _context;
-        public RoomService(IRoomDataAccess roomDataAccess)
+        private readonly IResponseModelFactory<Room> _responseModel;
+
+        public RoomService(IRoomDataAccess roomDataAccess, IResponseModelFactory<Room> responseModel)
         {
             _context = roomDataAccess;
+            _responseModel = responseModel;
         }
 
-        public ResponseModel GetRooms()
+        public ResponseModel<Room> GetRooms()
         {
             bool roomExists = _context.CheckRooms();
             if (roomExists) 
             {
                 List<Room> rooms = _context.GetRooms();
-                return new ResponseModel("Success", "The rooms successfully returned.", rooms);
+                return _responseModel.CreateResponseModel("Success", "The rooms successfully returned.", rooms);
             }
             else
             {
-                return new ResponseModel("NotFound", "Rooms is doesnt exists.");
+                return _responseModel.CreateResponseModel("NotFound", "Rooms is doesnt exists.");
             }
         }
-        public ResponseModel GetRoom(int id)
+        public ResponseModel<Room> GetRoom(int id)
         {
             bool roomExists = _context.CheckRoom(id);
             if (roomExists)
             {
                 Room room = _context.GetRoom(id);
-                return new ResponseModel("Success", "The room successfully returned.", room);
+                return _responseModel.CreateResponseModel("Success", "The room successfully returned.", room);
             }
             else
             {
-                return new ResponseModel("NotFound", "The room is doesnt exists.");
+                return _responseModel.CreateResponseModel("NotFound", "The room is doesnt exists.");
             }
 
         }
-        public ResponseModel AddRoom(Room room)
+        public ResponseModel<Room> AddRoom(Room room)
         {
             bool roomExists = _context.CheckRoom(room.Id);
             if (roomExists)
             {
-                return new ResponseModel("Conflict", "The room already exists");
+                return _responseModel.CreateResponseModel("Conflict", "The room already exists");
             }
             else
             {
                 _context.AddRoom(room);
-                return new ResponseModel("Created", "The room successfully added.");
+                return _responseModel.CreateResponseModel("Created", "The room successfully added.");
             }
         }
 
-        public ResponseModel UpdateRoom(Room room)
+        public ResponseModel<Room> UpdateRoom(Room room)
         {
             bool roomExists = _context.CheckRoom(room.Id);
             if (roomExists)
             {
                 _context.UpdateRoom(room);
-                return new ResponseModel("Updated", "The room successfully updated.");
+                return _responseModel.CreateResponseModel("Updated", "The room successfully updated.");
             }
             else
             {
-                return new ResponseModel("NotFound", "The room doesnt exists.");
+                return _responseModel.CreateResponseModel("NotFound", "The room doesnt exists.");
             }
 
         }
 
-        public ResponseModel RemoveRoom(int id)
+        public ResponseModel<Room> RemoveRoom(int id)
         {
             bool roomExists = _context.CheckRoom(id);
             if (roomExists)
             {
                 _context.RemoveRoom(id);
-                return new ResponseModel ("OK", "Room successfully deleted.");
+                return _responseModel.CreateResponseModel("OK", "Room successfully deleted.");
             }
             else
             {
-                return new ResponseModel("NotFound", "The room is doesnt exists.");
+                return _responseModel.CreateResponseModel("NotFound", "The room is doesnt exists.");
             }
         }
 
