865dc2c89fa1423af38a8e71aedebfc4bf890a63
Dfulop98 <dobo.fulop@icloud.com>
2023-03-15 14:40:42 +0100

    Add: slide cards component to home page

---
+++ ApartmanFrontend/src/app/home-slide-cards/home-slide-cards.component.ts
@@ -0,0 +1,54 @@
+import { Component,Input, OnInit } from '@angular/core';
+import Swiper, { SwiperOptions } from 'swiper';
+import { RoomService } from '../services/room.service';
+
+@Component({
+  selector: 'app-home-slide-cards',
+  templateUrl: './home-slide-cards.component.html',
+  styleUrls: ['./home-slide-cards.component.css']
+})
+
+export class HomeSlideCardsComponent implements OnInit {
+  @Input() activeIndex?: number;
+  swiper?: Swiper;
+
+  rooms: any[] = [];
+  
+  constructor(private roomService: RoomService) {}
+
+  ngOnInit(): void {
+    this.fetchRooms();
+    this.initSwiper();
+  }
+
+  fetchRooms(): void {
+    this.roomService.getRooms().subscribe(
+      (data: unknown) => {
+        if (typeof data === 'object' && data !== null && 'models' in data) {
+          this.rooms = (data as any).models;
+          console.log((data as any).models);
+        } else {
+          console.error('Unexpected data format:', data);
+        }
+      },
+      (error) => {
+        console.error('Error fetching rooms:', error);
+      }
+    );
+  }
+
+  initSwiper(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+      centeredSlides: true,
+      touchMoveStopPropagation: false,
+      touchReleaseOnEdges: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      }
+      
+    });
+  }
+}
