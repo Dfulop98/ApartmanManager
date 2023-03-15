5b4f62988707bf87d4c5fb412275f9d8e2ef57f8
Dfulop98 <dobo.fulop@icloud.com>
2023-03-15 10:58:37 +0100

    Refactor: change controller return types

--- ApartmanManagerApi/Controllers/RoomController.cs
+++ ApartmanManagerApi/Controllers/RoomController.cs
@@ -15,34 +15,33 @@ namespace ApartmanManagerApi.Controllers
         }
 
         [HttpGet("/api/room")]
-        public async Task<ActionResult<IEnumerable<Room>>> GetAllRooms()
+        public OkObjectResult GetAllRooms()
         {
-            IEnumerable<Room> allRooms = await _roomService.GetAllRoomAsync();
-            return Ok(allRooms);
+            return Ok(_roomService.GetRooms());
         }
 
         [HttpGet("/api/room/{id}")]
-        public async Task<ActionResult<Room>> GetRoomById(int id)
+        public OkObjectResult GetRoomById(int id)
         {
-            return Ok(await _roomService.GetRoomByIdAsync(id));
+            return Ok(_roomService.GetRoom(id));
         }
 
         [HttpPost("/api/room/add")]
-        public async Task<ActionResult> AddRoom([FromBody] Room room)
+        public OkObjectResult AddRoom([FromBody] Room room)
         {
-            return Ok(await _roomService.AddRoomAsync(room));
+            return Ok(_roomService.AddRoom(room));
         }
 
         [HttpPut("/api/room/update")]
-        public async Task<ActionResult> UpdateRoom([FromBody] Room room)
+        public OkObjectResult UpdateRoom([FromBody] Room room)
         {
-            return Ok(await _roomService.UpdateRoomAsync(room));
+            return Ok(_roomService.UpdateRoom(room));
         }
 
         [HttpDelete("/api/room/remove/{id}")]
-        public async Task<ActionResult> DeleteRoom(int id)
+        public OkObjectResult DeleteRoom(int id)
         {
-            return Ok(await _roomService.RemoveRoomByIdAsync(id));
+            return Ok(_roomService.RemoveRoom(id));
         }
 
 
