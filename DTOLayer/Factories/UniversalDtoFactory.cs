4fd986026a8c3a5a6502ff414d4ac1e4372be692
Dfulop98 <dobo.fulop@icloud.com>
2023-03-17 00:02:22 +0100

    Feat: generic Dto factory for convert any model to dto one

---
+++ DTOLayer/Factories/UniversalDtoFactory.cs
@@ -0,0 +1,38 @@
+﻿using DTOLayer.Models;
+
+namespace DTOLayer.Factories
+{
+    public class UniversalDtoFactory
+    {
+        public static UniversalDTO CreateFromObject(object obj, IEnumerable<string> propertyNames)
+        {
+            var dto = new UniversalDTO();
+            var type = obj.GetType();
+
+            foreach (var propertyName in propertyNames)
+            {
+                var property = type.GetProperty(propertyName);
+                if (property != null)
+                {
+                    var value = property.GetValue(obj);
+                    dto.SetProperty(propertyName, value);
+                }
+            }
+
+            return dto;
+        }
+        public static List<UniversalDTO> CreateListFromObjects<T>(IEnumerable<T> objects, List<string> propertiesToInclude = null)
+        {
+            var dtoList = new List<UniversalDTO>();
+
+            foreach (T obj in objects)
+            {
+                UniversalDTO dto = CreateFromObject(obj, propertiesToInclude);
+                dtoList.Add(dto);
+            }
+
+            return dtoList;
+        }
+    }
+
+}
---
+++ DTOLayer/Models/UniversalDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace DTOLayer.Models
+{
+    public class UniversalDTO
+    {
+        public Dictionary<string, object> Properties { get; set; }
+
+        public UniversalDTO()
+        {
+            Properties = new Dictionary<string, object>();
+        }
+
+        public T GetProperty<T>(string propertyName)
+        {
+            if (Properties.TryGetValue(propertyName, out object value))
+            {
+                return (T)value;
+            }
+
+            return default(T);
+        }
+
+        public void SetProperty<T>(string propertyName, T value)
+        {
+            Properties[propertyName] = value;
+        }
+    }
+
+}
