67f3969601b738287331efbbb0a255b9bb726326
Dfulop98 <dobo.fulop@icloud.com>
2023-03-19 13:18:44 +0100

    Add: DTO config , and common files

---
+++ DTOLayer/Configurations/DTOConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.Configurations
+{
+    public static class DTOConfig
+    {
+        public static List<string> RoomProperties { get; } = new List<string>
+        {
+            "Id",
+            "RoomNumber",
+            "Capacity",
+            "IsAvailable",
+            "PricePerNight",
+            "Description",
+            "Images"
+        };
+
+        public static Dictionary<string, IEnumerable<string>> RoomIncludedProperties { get; } = new Dictionary<string, IEnumerable<string>>
+        {
+            { "Images", new List<string> { "Url" } }
+        };
+
+
+    }
+}
