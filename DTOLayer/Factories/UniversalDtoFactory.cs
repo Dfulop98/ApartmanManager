b0a75d9b848e0270c0c8d265e4220df8fa2f1dc6
Dfulop98 <dobo.fulop@icloud.com>
2023-03-19 13:19:13 +0100

    Update: select item with connected table properties

--- DTOLayer/Factories/UniversalDtoFactory.cs
+++ DTOLayer/Factories/UniversalDtoFactory.cs
@@ -1,10 +1,11 @@
 ﻿using DTOLayer.Models;
+using System.Collections;
 
 namespace DTOLayer.Factories
 {
     public class UniversalDtoFactory
     {
-        public static UniversalDTO CreateFromObject(object obj, IEnumerable<string> propertyNames)
+        public static UniversalDTO CreateFromObject(object obj, IEnumerable<string> propertyNames, IDictionary<string, IEnumerable<string>> includedProperties = null)
         {
             var dto = new UniversalDTO();
             var type = obj.GetType();
@@ -15,19 +16,38 @@ namespace DTOLayer.Factories
                 if (property != null)
                 {
                     var value = property.GetValue(obj);
-                    dto.SetProperty(propertyName, value);
+                    if (value is IEnumerable && !(value is string))
+                    {
+                        var collection = (IEnumerable)value;
+                        var list = new List<UniversalDTO>();
+                        foreach (var item in collection)
+                        {
+                            list.Add(CreateFromObject(item, includedProperties != null && includedProperties.ContainsKey(propertyName) ? includedProperties[propertyName] : item.GetType().GetProperties().Select(p => p.Name)));
+                        }
+                        dto.SetProperty(propertyName, list);
+                    }
+                    else
+                    {
+                        dto.SetProperty(propertyName, value);
+                    }
                 }
             }
 
             return dto;
         }
-        public static List<UniversalDTO> CreateListFromObjects<T>(IEnumerable<T> objects, List<string> propertiesToInclude = null)
+
+        public static List<UniversalDTO> CreateListFromObjects<T>(IEnumerable<T> objects, List<string> propertiesToInclude = null, IDictionary<string, IEnumerable<string>> includedProperties = null)
         {
             var dtoList = new List<UniversalDTO>();
 
+            if (objects == null)
+            {
+                throw new ArgumentNullException(nameof(objects), "Objects parameter must not be null.");
+            }
+
             foreach (T obj in objects)
             {
-                UniversalDTO dto = CreateFromObject(obj, propertiesToInclude);
+                UniversalDTO dto = CreateFromObject(obj, propertiesToInclude, includedProperties);
                 dtoList.Add(dto);
             }
 
