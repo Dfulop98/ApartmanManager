1dd439651277c3ff23b80b91dc2aef83cc4b07a8
Dfulop98 <dobo.fulop@icloud.com>
2023-05-03 23:08:34 +0200

    Feat: generic controller interface

---
+++ ApartmanManagerApi/Interfaces/IGenericClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmanManagerApi.Interfaces
+{
+    public interface IGenericClientController
+    {
+        IActionResult GetAllOperations(string type);
+        IActionResult AddOperations(string type, dynamic entity);
+        IActionResult DeleteOperations(string type, int id);
+        IActionResult UpdateOperations(string type, int id, dynamic entity);
+    }
+}
