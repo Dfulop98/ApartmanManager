d3f1b485cfa3137d9dfab67f938f1309e6405aec
Dfulop98 <dobo.fulop@icloud.com>
2023-06-08 22:28:11 +0200

    Feat: add image endpoint

--- ApartmanManagerApi/Controllers/ImageController.cs
+++ ApartmanManagerApi/Controllers/ImageController.cs
@@ -13,10 +13,12 @@ namespace ApartmanManagerApi.Controllers
     public class ImagesController : ControllerBase
     {
         private readonly IImagesService _imagesService;
+        private readonly IRoomService _roomService;
 
-        public ImagesController(IImagesService imagesService)
+        public ImagesController(IImagesService imagesService, IRoomService roomService)
         {
             _imagesService = imagesService;
+            _roomService = roomService;
         }
 
         [HttpGet]
@@ -41,26 +43,60 @@ namespace ApartmanManagerApi.Controllers
             return BadRequest(result.ErrorMessage);
         }
 
-        [HttpGet("room/{id}")]
-        public IActionResult GetImagesByRoomId(int id)
-        {
-            var result = _imagesService.GetImagesByRoomId(id);
-            if (result.IsSuccess)
-            {
-                return Ok(result.Data);
-            }
-            return BadRequest(result.ErrorMessage);
-        }
+        //[HttpGet("room/{id}")]
+        //public IActionResult GetImagesByRoomId(int id)
+        //{
+        //    var result = _imagesService.GetImagesByRoomId(id);
+        //    if (result.IsSuccess)
+        //    {
+        //        return Ok(result.Data);
+        //    }
+        //    return BadRequest(result.ErrorMessage);
+        //}
 
         [HttpPost]
-        public IActionResult AddImage([FromBody] Stream imageStream, [FromQuery] string imageName, [FromQuery] string type)
+        public IActionResult AddImage(IFormFile image, string type, int? roomId)
         {
+            Stream imageStream = image.OpenReadStream();
+            string imageName = image.FileName;
+
             var result = _imagesService.AddImage(imageStream, imageName, type);
-            if (result.IsSuccess)
+            
+            if(type == "room")
             {
-                return Created("", result.Data);
+                if (roomId != null)
+                {
+                    //UniversalDTO relatedRoomDTO = _roomService.GetRoom((int)roomId).Data;
+                    //Room relatedRoom = new()
+                    //{
+                    //    Id = relatedRoomDTO.GetProperty<Room>("Id").Id,
+                    //    RoomNumber = relatedRoomDTO.GetProperty<Room>("RoomNumber").RoomNumber,
+                    //    Capacity = relatedRoomDTO.GetProperty<Room>("Capacity").Capacity,
+                    //    IsAvailable = relatedRoomDTO.GetProperty<Room>("IsAvailable").IsAvailable,
+                    //    PricePerNight = relatedRoomDTO.GetProperty<Room>("PricePerNight").PricePerNight,
+                    //    Description = relatedRoomDTO.GetProperty<Room>("Description").Description,
+                    //    Reservations = relatedRoomDTO.GetProperty<Room>("Reservations").Reservations,
+                    //    Images = relatedRoomDTO.GetProperty<Room>("Images").Images,
+                    //};
+                    var linkImageResponse = _roomService.LinkImageToRoom(result.Data, (int)roomId);
+                    if (linkImageResponse.IsSuccess)
+                    {
+                        return Created("Link to room and upload", result.Data);
+                    }
+                    return BadRequest(linkImageResponse.ErrorMessage);
+                }
+                return BadRequest("Room id is null while linking photo");
+                
+            }
+            if (result.IsSuccess)
+            {    
+                    return Created("", result.Data);
             }
             return BadRequest(result.ErrorMessage);
+            
+            
+
+
         }
         
     }
