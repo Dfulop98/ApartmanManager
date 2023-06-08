0c0b732f21fed595ca0da09c22d31904af164d39
Dfulop98 <dobo.fulop@icloud.com>
2023-06-08 22:25:45 +0200

    Add: getroom, getroom images fetches

---
+++ apartman-frontend/lib/getRoomImages.tsx
@@ -0,0 +1,9 @@
+
+export default async function getRoom(roomId :string) {
+
+    const res = await fetch(`http://localhost:5008/api/room/${roomId}`);
+  
+    if(!res.ok) throw new Error('failed to fetch');
+    return res.json()
+  }
+  
