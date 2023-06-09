60615464299335ecf00f99bd4e79fed830e483e8
Dfulop98 <dobo.fulop@icloud.com>
2023-06-09 23:26:52 +0200

    Refactor: take out some redundant html dom

--- apartman-frontend/components/roomCard/roomCard.tsx
+++ apartman-frontend/components/roomCard/roomCard.tsx
@@ -3,6 +3,7 @@ import { Room } from "@/types";
 import Link from "next/link";
 import Styles from "./RoomCard.module.css";
 import Carousel from '../carousel/carousel';
+import cn from "classnames";
 export default function RoomCard({params} : {params : Room[]}) {
 
   return (
@@ -15,30 +16,27 @@ export default function RoomCard({params} : {params : Room[]}) {
                     <div className={Styles.room_images}>
                         <Carousel params={room.properties.Images} />
                     </div>
-                    <div className={Styles.room_info}>
-                        <div className={Styles.info_wrapper}>
-                            <div className={Styles.info_header}>
-                                <Link href={`/rooms/${room.properties.Id}`}>Room{room.properties.RoomNumber}</Link>
-                            </div>
-                            <div className={Styles.info_body}>
-                                <h1>Description:</h1>
-                                <p>{room.properties.Description}</p>
-                            </div>
+                    <div className={cn(Styles.room_info,Styles.info_wrapper)}>
+                        <div className={Styles.info_header}>
+                            <Link href={`/rooms/${room.properties.Id}`}>Room{room.properties.RoomNumber}</Link>
+                        </div>
+                        <div className={Styles.info_body}>
+                            <h1>Description:</h1>
+                            <p>{room.properties.Description}</p>
+                        </div>
 
-                            <div className={Styles.info_footer}>
-                                <div className={Styles.footer_wrapper}>
-                                    <div className={Styles.footer_details}>
-                                        <h1>Capacity:</h1>
-                                        <p>{room.properties.Capacity}</p>
-                                        <br />
-                                        <h1>Price:</h1>
-                                        <p>{room.properties.PricePerNight}&#8364;/person</p>
-                                    </div>
-                                    <div className={Styles.footer_buttons}>
-                                        <Link href={`/callendar/${room.properties.Id}`}>Book Now</Link>
-                                        <Link href={`/rooms/${room.properties.Id}`}>Calendar</Link>
-                                    </div>
-                                </div>
+                        <div className={cn(Styles.info_footer,Styles.footer_wrapper)}>
+                            
+                            <div className={Styles.footer_details}>
+                                <h1>Capacity:</h1>
+                                <p>{room.properties.Capacity}</p>
+                                <br />
+                                <h1>Price:</h1>
+                                <p>{room.properties.PricePerNight}&#8364;/person</p>
+                            </div>
+                            <div className={Styles.footer_buttons}>
+                                <Link href={`/callendar/${room.properties.Id}`}>Book Now</Link>
+                                <Link href={`/rooms/${room.properties.Id}`}>Calendar</Link>
                             </div>
                         </div>
                     </div>
