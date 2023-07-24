a5d18c6c5d47ff30d79cf46835d75335f2251b81
Dfulop98 <dobo.fulop@icloud.com>
2023-07-24 16:30:09 +0200

    Add: carousel component and style

--- apartman-frontend/components/carousel/carousel.tsx
+++ apartman-frontend/components/carousel/carousel.tsx
@@ -1,23 +1,25 @@
 "use client"
 import { useState } from "react";
 import { ImageType } from "@/types";
-import Styles from "./Carousel.module.css";
 import Image from "next/image";
 import cn from "classnames";
 
 interface CarouselProps {
-  params: ImageType[];
+  params:{
+    images: ImageType[];
+    styles: any;
+  } 
 }
 
 export default function Carousel({ params }: CarouselProps) {
   const [activeIndex, setActiveIndex] = useState(0);
-
+  const {images, styles}  = params;
   const handleButtonClick = (offset: number) => {
-    if(activeIndex+offset > params.length-1){
+    if(activeIndex+offset > images.length-1){
         setActiveIndex(0);
     }
     else if(activeIndex+offset < 0){
-        setActiveIndex(params.length-1);
+        setActiveIndex(images.length-1);
     }
     else{
         setActiveIndex(activeIndex+offset);
@@ -29,30 +31,33 @@ export default function Carousel({ params }: CarouselProps) {
 
   return (
     <section aria-label="Newest Photos">
-      <div className={Styles.carousel} data-carousel>
+      <div className={styles.carousel_background}></div>
+      <div className={styles.carousel} data-carousel>
         
-        <button
-          className={cn(Styles.carousel_button, Styles.prev)}
-          onClick={() => handleButtonClick(-1)}
-          >&#8701;</button>
 
-        <button
-          className={cn(Styles.carousel_button, Styles.next)}
-          onClick={() => handleButtonClick(1)}
-        >&#8702;</button>
+          <button
+            className={cn(styles.carousel_button, styles.prev)}
+            onClick={() => handleButtonClick(-1)}
+            >&#8701;</button>
 
-        <ul data-slides>
-          {params.map((image, index) => (
-            <li
-              className={cn(Styles.slide, { [Styles.active]: index === activeIndex })}
-              key={image.properties.Id}
-              data-active={index === activeIndex ? "" : undefined}
-            >
-              <Image src={image.properties.Url} alt={`room_${image.properties.Id}`} fill />
-            </li>
-          ))}
-        </ul>
-      </div>
+          <button
+            className={cn(styles.carousel_button, styles.next)}
+            onClick={() => handleButtonClick(1)}
+          >&#8702;</button>
+
+          <ul data-slides>
+            {images.map((image, index) => (
+              <li
+                className={cn(styles.slide, { [styles.active]: index === activeIndex })}
+                key={image.properties.Id}
+                data-active={index === activeIndex ? "" : undefined}
+              >
+                <Image src={image.properties.Url} alt={`room_${image.properties.Id}`} fill />
+              </li>
+            ))}
+          </ul>
+        </div>
+      
     </section>
   );
 }
